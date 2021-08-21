4dc5316baeb034c3987ed2efca3a3e9bebe7ed74
ivaquero <46377665+integzz@users.noreply.github.com>
2021-08-21 14:38:28 +0800

    📝 update docs
    Signed-off-by: ivaquero <46377665+integzz@users.noreply.github.com>

--- galleries/g_ml/rank1d.py
+++
@@ -1,8 +0,0 @@
-from yellowbrick.datasets import load_credit
-from yellowbrick.features import Rank1D
-
-X, y = load_credit()
-
-visualizer = Rank1D(algorithm="shapiro")
-visualizer.fit_transform(X)
-visualizer.show()
